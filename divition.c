10986e23533d01e39ef09e3acd56e6a2b7002b6d
Noshin1234 <149030394+Noshin1234@users.noreply.github.com>
2023-11-08 20:12:36 +0600

    Update divition.c

--- divition.c
+++ divition.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main(){
-    float a,b,divition,sub;
+    float a,b,divition,sub,add;
     scanf("%f %f",&a,&b);
     divition=a/b;
 sub=a-b;
+    add=a+b;
     printf("%f",divition);
     print("%f",sub);
+    print("%f",add);
 }
